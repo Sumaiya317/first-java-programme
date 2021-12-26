fc449bcd86d53462b13e146f9ed2e28c45d07ef2
somaiya <somaiyaalam4444@gmail.com>
2021-12-26 12:27:50 +0600

    added new line

--- hello.java
+++ hello.java
@@ -6,5 +6,7 @@ public class hello {
     public static void main(String[] args) {
         String a = "Kaber";
         System.out.print(a);
+        String r = "Raber";
+        System.out.print(r);
     }
 }
